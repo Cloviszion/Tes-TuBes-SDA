4ab479f89d249d93df0db53dc571b6745f201ca9
Cloviszion <rmfarhanfauzan@gmail.com>
2025-06-09 16:43:23 +0700

    Update tree.h

--- tree.h
+++ tree.h
@@ -20,13 +20,14 @@ TreeNode* Tree_createNode(const char* name, const char* type);
 TreeNode* Tree_parseJSON(cJSON* json);
 void Tree_addNode(TreeNode* parent, TreeNode* newNode);
 TreeNode* Tree_findNode(TreeNode* node, const char* name);
-void Tree_deleteNode(TreeNode** root, const char* name, void (*addToHistory)(HistoryNode**, const char*), void (*pushDeleteStack)(StackNode**, TreeNode*, const char*));
+TreeNode* Tree_findParent(TreeNode* node, TreeNode* target); // Fungsi baru untuk mencari parent
+void Tree_deleteNode(TreeNode** root, const char* name, void (*addToHistory)(HistoryNode**, const char*), void (*pushDeleteStack)(StackNode**, TreeNode*, TreeNode*, const char*));
 void Tree_editNode(TreeNode* node, const char* name, const char* newName, const char* newType, void (*addToHistory)(HistoryNode**, const char*));
 void Tree_searchNode(TreeNode* node, const char* name);
-void Tree_displayPreorder(TreeNode* node, int level);
+void Tree_displayPreorder(TreeNode* node, int level, int* isLast);
 void Tree_displayLevelOrder(TreeNode* root);
 void Tree_displaySubtree(TreeNode* node, const char* name);
-void Tree_calculateStats(TreeNode* node, int* stats);
+void Tree_calculateStats(TreeNode* node, int* stats); // Array stats sekarang mencakup kabupaten
 void Tree_saveToJSON(TreeNode* node, cJSON* json);
 void Tree_free(TreeNode* node);
 
